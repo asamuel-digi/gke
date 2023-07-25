a312e2e0b4340c1da3317f35f4326d3c65dc7392
asamuel-digi <83590864+asamuel-digi@users.noreply.github.com>
2023-07-25 14:34:01 +0530

    added instance info

--- src/main/java/com/abhisheksamuely/gcp/gke/controller/GKEController.java
+++ src/main/java/com/abhisheksamuely/gcp/gke/controller/GKEController.java
@@ -1,12 +1,17 @@
 package com.abhisheksamuely.gcp.gke.controller;
 
 import java.util.Calendar;
-import java.text.SimpleDateFormat;  
+import java.text.SimpleDateFormat;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GKEController {
+	
+	@Autowired
+	private InstanceInformationService service;
 
 	@GetMapping("/message")
 	public String message() {
@@ -17,6 +22,6 @@ public class GKEController {
 		
 		System.out.println("application hit at "+sdf.format(calndr.getTime()));
 		System.out.println("hello abu, arun");
-		return "application hit at "+sdf.format(calndr.getTime())+" arun and abu";
+		return "application hit at "+sdf.format(calndr.getTime())+" instance: "+service.retrieveInstanceInfo();
 	}
 }
