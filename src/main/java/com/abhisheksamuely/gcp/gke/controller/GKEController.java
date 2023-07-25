a38fb73b36fd44a97b2f1e0d1e1d84a24893f93e
asamuel-digi <83590864+asamuel-digi@users.noreply.github.com>
2023-07-25 14:49:07 +0530

    update instance id code

--- src/main/java/com/abhisheksamuely/gcp/gke/controller/GKEController.java
+++ src/main/java/com/abhisheksamuely/gcp/gke/controller/GKEController.java
@@ -1,27 +1,22 @@
 package com.abhisheksamuely.gcp.gke.controller;
 
-import java.util.Calendar;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GKEController {
 	
-	@Autowired
-	private InstanceInformationService service;
-
 	@GetMapping("/message")
 	public String message() {
 		Calendar calndr = Calendar.getInstance();
 		calndr.setTimeInMillis(System.currentTimeMillis());
 		SimpleDateFormat sdf = new SimpleDateFormat("dd:MM:yy HH:mm:ss");
 		
+		String instanceId = com.google.cloud.MetadataConfig.getInstanceId();
 		
-		System.out.println("application hit at "+sdf.format(calndr.getTime()));
-		System.out.println("hello abu, arun");
-		return "application hit at "+sdf.format(calndr.getTime())+" instance: "+service.retrieveInstanceInfo();
+		return "application hit at "+sdf.format(calndr.getTime())+" instance: "+instanceId;
 	}
 }
